2f8085f8b726f2bd166ffbbf830acaebf017415e
Will Hodgson <will.hodgson@current-rms.com>
2019-07-25 14:07:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,10 @@ def countdown(seconds)
 end
 
 def countdown_with_sleep(seconds)
-
+  while seconds > 0
+    sleep(1.second)
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
